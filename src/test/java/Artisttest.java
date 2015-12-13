99d1c0d5d0710237bf90f63395722d041bf97a5a
muvvva566 <muvvva566@gmail.com>
2015-12-13 15:37:27 +0000

    maven new1 java commit

---
+++ src/test/java/Artisttest.java
@@ -0,0 +1,17 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by narasimharaomuvva on 13/12/2015.
+ */
+public class Artisttest {
+
+        @Test
+        public void artisttest()
+        {
+            Assert.assertTrue(true);
+        }
+
+
+}
